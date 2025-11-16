cfea7eeb44f9629bb50a6f4595e4d74a726da23f
Filippo Dembech <filippo.dembech@gmail.com>
2025-11-16 08:34:13 +0100

    fix: Button width glitch

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -27,7 +27,7 @@ export default function Button({
     return (
         <div>
             <button
-                className="flex items-center px-2 py-1 text-white bg-red-500 rounded-lg cursor-pointer hover:bg-red-600 transition-colors duration-400"
+                className="flex items-center px-2 py-1 text-white bg-red-500 rounded-lg cursor-pointer hover:bg-red-600 transition-colors duration-400 w-full"
                 {...htmlButtonAttributes}
             >
                 {icon && <span className="text-white mr-2 transition-colors duration-400">{icon}</span>}
